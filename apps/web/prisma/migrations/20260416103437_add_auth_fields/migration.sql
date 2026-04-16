2e87381554ab8275bd3d1528281b2e9050c9056b
Sergey <drobilenko94@mail.ru>
2026-04-16 13:34:52 +0300

    chore: prisma миграция для добавления email и AuthProvider

---
+++ apps/web/prisma/migrations/20260416103437_add_auth_fields/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "authProvider" "AuthProvider" NOT NULL DEFAULT 'TELEGRAM',
+ADD COLUMN     "displayName" TEXT,
+ADD COLUMN     "email" TEXT,
+ADD COLUMN     "isBanned" BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
